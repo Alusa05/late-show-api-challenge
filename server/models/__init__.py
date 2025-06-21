8c9a5e87f8f28baf7722d75d65a18653c5138403
Lisa Alusa <sayilisaalusa@gmail.com>
2025-06-21 17:31:31 +0300

    made changes

---
+++ server/models/__init__.py
@@ -0,0 +1,5 @@
+from server.app import db
+from server.models.guest import Guest
+from server.models.episode import Episode
+from server.models.appearance import Appearance
+from server.models.user import User
