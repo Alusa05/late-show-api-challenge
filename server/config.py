0393a26f4628f6a464a54f9949a12322335dfe54
Lisa Alusa <sayilisaalusa@gmail.com>
2025-06-21 17:28:15 +0300

    made changes to config file

--- server/config.py
+++ server/config.py
@@ -1,6 +1,5 @@
 import os
 
 SQLALCHEMY_DATABASE_URI = "postgresql://alusa:051598@localhost:5432/late_show_db"
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = os.environ.get('SECRET_KEY', 'dev')
-JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'super-secret')
+JWT_SECRET_KEY = os.environ.get = "super-secret-key"
+
